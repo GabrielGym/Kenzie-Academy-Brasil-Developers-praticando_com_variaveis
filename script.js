cae7af2a88894ecbcedc6485c3d94ecd9ad833af
Gabriel_ggm <biel.ferreira12342@gmail.com>
2022-10-12 11:22:05 -0300

    Praticando com variaveis

---
+++ script.js
@@ -0,0 +1,68 @@
+// Exercicio 1 //
+
+
+/* let precoGasolina = 6.80;
+let dinheiroDoCliente = parseFloat(prompt("Digite quanto de dinheiro você vai abastecer"));
+let convercaoEmGasolina = dinheiroDoCliente / precoGasolina;
+
+alert(`O Cliente abasteceu ${convercaoEmGasolina}L`); */
+
+
+// Exercicio 2 //
+
+
+/* let grausCelsius = parseFloat(prompt("Digite a temperatura em graus celsius"));
+let fahrenheit = (9 * grausCelsius + 160) / 5;
+
+alert(`O valor ${grausCelsius}ºc em Fahrenheit é ${fahrenheit}`); */
+
+// Exercicio 3 //
+
+
+/* let fahrenheit = parseFloat(prompt("Digite a temperatura em Fahrenheit"));
+let grausCelsius = ((fahrenheit - 32) * 5) / 9;
+
+alert(`O valor ${fahrenheit}ºf em Celsius é ${grausCelsius}`); */
+
+
+// Exercicio 4 //
+
+
+/* let comprimento = parseFloat(prompt("Digite o comprimento"));
+let largura = parseFloat(prompt("Digite a largura"));
+let altura = parseFloat(prompt("Digite a altura"));
+let volume = comprimento * largura * altura;
+
+alert(`O volume é de: ${volume}m3`); */
+
+
+// Exercicio 5 //
+
+
+/* let valorInteiro = parseInt(prompt("Digite o valor inteiro"));
+let valorAoQuadrado = valorInteiro * valorInteiro;
+
+alert(`O valor ${valorInteiro} ao quadrado é ${valorAoQuadrado}`); */
+
+// Exercicio 6 //
+
+
+/* let valor1 = parseFloat(prompt("Digite um número"));
+let valor2 = parseFloat(prompt("Digite um número"));
+let valor3 = parseFloat(prompt("Digite um número"));
+let valor4 = parseFloat(prompt("Digite um número"));
+let valor5 = parseFloat(prompt("Digite um número"));
+let somaDosNumeros = valor1 + valor2 + valor3 + valor4 + valor5;
+let mediaDaSoma = somaDosNumeros / 5;
+
+alert(`A soma dos valores informados é: ${somaDosNumeros} e a média das notas é: ${mediaDaSoma}`); */
+
+
+// Exercicio 7 //
+
+
+let valorInteiro1 = parseInt(prompt("Digite um numero positivo"));
+let valorInteiro2 = parseInt(prompt("Digite outro numero positivo"));
+let restoDaDivisao = valorInteiro1 / valorInteiro2;
+
+alert(`O resto da divisão de ${valorInteiro1} por ${valorInteiro2} é: ${restoDaDivisao}`);
